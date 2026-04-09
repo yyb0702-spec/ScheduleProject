77404d86976c9e3ff2c84c28b38900227c8849b7
깡단 <yyb19920702@gmail.com>
2026-04-09 16:45:09 +0900

    feat : 챕터2 read 조회응답 dto 추가

---
+++ src/main/java/com/example/schedule/dto/GetOneScheduleResponse.java
@@ -0,0 +1,25 @@
+package com.example.schedule.dto;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class GetOneScheduleResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String name;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+
+    public GetOneScheduleResponse(Long id, String title, String content, String name, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.name = name;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+}
